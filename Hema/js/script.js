1fee7b50a20fec957b219be7355ed25f21ab3ce7
Leon Westerveld <35258@ma-web.nl>
2023-06-07 23:27:57 +0200

    productpage colorchanger + review docenten

--- Hema/js/script.js
+++ Hema/js/script.js
@@ -3,9 +3,8 @@ function myFunction() {
     let x = document.getElementById("myLinks");
     if (x.style.display === "block") {
         x.style.display = "none";
-    }
-    else {
-    x.style.display = "block";
+    } else {
+        x.style.display = "block";
     }
 }
 
@@ -14,9 +13,9 @@ let arrowButtons = document.getElementsByClassName("arrow");
 let reviews = document.getElementsByClassName("review");
 let modus = "one-two-three";
 
-for(let i = 0; i < arrowButtons.length; i++){
-    arrowButtons[i].onclick = function(){
-        if(modus === "one-two-three"){
+for (let i = 0; i < arrowButtons.length; i++) {
+    arrowButtons[i].onclick = function () {
+        if (modus === "one-two-three") {
             reviews[0].style.display = "none";
             reviews[1].style.display = "none";
             reviews[2].style.display = "none";
@@ -24,8 +23,7 @@ for(let i = 0; i < arrowButtons.length; i++){
             reviews[4].style.display = "block";
             reviews[5].style.display = "block";
             modus = "four-five-six";
-        }
-        else{
+        } else {
             reviews[0].style.display = "block";
             reviews[1].style.display = "block";
             reviews[2].style.display = "block";
@@ -33,8 +31,8 @@ for(let i = 0; i < arrowButtons.length; i++){
             reviews[4].style.display = "none";
             reviews[5].style.display = "none";
             modus = "one-two-three";
-            }
-    }
+        }
+    };
 }
 
 let slideIndex = 1;
@@ -42,26 +40,59 @@ showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
 }
+
+
+let btn = document.getElementById("btn");
+let sb = document.getElementById("polo");
+
+btn.onclick = (event) => {
+    event.preventDefault();
+
+    value = sb.value;
+    switch (value) {
+        case "navy":
+            window.location.href = "polos.html";
+            break;
+
+        case "white":
+            window.location.href = "polos_white.html";
+            break;
+
+        case "red":
+            window.location.href = "polos_red.html";
+            break;
+
+        case "beige":
+            window.location.href = "polos_beige.html";
+            break;
+
+        case "pink":
+            window.location.href = "polos_pink.html";
+            break;
+
+        default:
+    }
+};
